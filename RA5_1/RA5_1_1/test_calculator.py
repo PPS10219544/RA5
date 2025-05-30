1176d157940780d6b7586c6231c4529c5a341022
PPS10219544 <carpernav8@alu.edu.gva.es>
2025-05-30 13:50:09 +0200

    Update test_calculator.py

--- RA5_1/RA5_1_1/test_calculator.py
+++ RA5_1/RA5_1_1/test_calculator.py
@@ -4,18 +4,15 @@ from calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
     def setUp(self):
         self.calc = Calculadora()
-
+        
     def test_multiplicar_enteros(self):
-        self.assertEqual(self.calc.multiplicar(3, 4), 12)
+        self.assertEqual(self.calc.multiplicar(2, 3), 999)  # Provocamos error
 
     def test_multiplicar_flotantes(self):
         self.assertAlmostEqual(self.calc.multiplicar(2.5, 4.0), 10.0)
 
     def test_multiplicar_por_cero(self):
         self.assertEqual(self.calc.multiplicar(0, 100), 0)
-
-    def test_multiplicar_enteros(self):
-    self.assertEqual(self.calc.multiplicar(2, 3), 999)  # Provocamos error
 
 if __name__ == '__main__':
     unittest.main()
