cfa867b3acb533e40b8ea04484a755a95d70e55b
PPS10219544 <carpernav8@alu.edu.gva.es>
2025-05-30 14:18:28 +0200

    Update test_calculator.py

--- RA5_1/RA5_1_1/test_calculator.py
+++ RA5_1/RA5_1_1/test_calculator.py
@@ -5,9 +5,11 @@ class TestCalculadora(unittest.TestCase):
     def setUp(self):
         self.calc = Calculadora()
         
+    #def test_multiplicar_enteros(self):
+    #    self.assertEqual(self.calc.multiplicar(2, 3), 999)  # Provocamos error
+    
     def test_multiplicar_enteros(self):
-        self.assertEqual(self.calc.multiplicar(2, 3), 999)  # Provocamos error
-
+        self.assertEqual(self.calc.multiplicar(3, 4), 12)
     def test_multiplicar_flotantes(self):
         self.assertAlmostEqual(self.calc.multiplicar(2.5, 4.0), 10.0)
 
